9fb29457ba6c8395e117c67bf3b26f6ac0379cd5
Osempu <49426233+Osempu@users.noreply.github.com>
2020-03-09 09:22:46 -0500

    Bootstrap classes added.

--- Stock_MVC/Startup.cs
+++ Stock_MVC/Startup.cs
@@ -43,7 +43,7 @@ namespace Stock_MVC
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
